3d8db43eb77d17188d1033395f79b18a1432dd1b
student <student@jev-trav>
2016-11-03 10:01:59 +0200

    comit main

--- mavenproject1/src/main/java/lv/smasys/controllers/MainController.java
+++ mavenproject1/src/main/java/lv/smasys/controllers/MainController.java
@@ -9,13 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
   @RequestMapping(value="/", method = RequestMethod.GET)
-  public String index() {
-    return "posts/login";
+  public ModelAndView index() {
+    return new ModelAndView("redirect:/posts");
   }
 
 }
