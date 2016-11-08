38fddc6391aca21266d2dfeb3d3f86ff0696bbbd
student <student@dan-zha>
2016-11-08 09:13:16 +0200

    Merge origin/master
    Conflicts:
    	mavenproject1/src/main/java/lv/smasys/controllers/TeacherController.java

--- mavenproject1/src/main/java/lv/smasys/controllers/LoginController.java
+++ mavenproject1/src/main/java/lv/smasys/controllers/LoginController.java
@@ -38,6 +38,7 @@ import org.slf4j.LoggerFactory;
  */
 @Controller
 public class LoginController {
+
     @Autowired
     StudentRepository studentRepository;
     @Autowired
@@ -51,73 +52,113 @@ public class LoginController {
     @Autowired
     CourseRepository courseRepository;
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-    
+
+    public static boolean isName(String name) {
+        boolean status = false;
+        char[] chars = name.toCharArray();
+
+        if (Character.isUpperCase(chars[0])) {
+            for (char c : chars) {
+                if (Character.isLetter(c)) {
+                    status = true;
+                }
+            }
+        }
+        return status;
+    }
+
+    public static boolean isTelNumber(String telephoneNumber) {
+        boolean status = false;
+        if (telephoneNumber.matches("[2]{1}\\d{7}")
+                || (telephoneNumber.matches("[3][7][1][2]\\d{7}"))
+                || (telephoneNumber.matches("[+][3][7][1][2]\\d{7}"))) {
+            status = true;
+        }
+        System.out.println("Number=" + status);
+        return status;
+    }
+
+    public static boolean isEmail(String email) {
+        boolean status = false;
+        if (email.matches(".+@.+\\.[a-z]+")) {
+            status = true;
+        }
+        System.out.println("Email=" + status);
+        return status;
+    }
+
 //    //@RequestMapping("/{userId}/bookmarks")
 //    @RequestMapping(value="/",method=RequestMethod.POST)
 //    public String success() {
 //        return "redirect:posts/list";
 //    }
 //    
-    @RequestMapping(value="/",method=RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "posts/login";
     }
-    @RequestMapping(value="/createuser",method=RequestMethod.GET)
+
+    @RequestMapping(value = "/createuser", method = RequestMethod.GET)
     public String newUser(Model model) {
         List<Roles> list = new ArrayList<Roles>();
         list.add(new Roles("ROLE_USER"));
         list.add(new Roles("ROLE_ADMIN"));
-        model.addAttribute("userroles",list);
+        model.addAttribute("userroles", list);
         return "createuser";
     }
-    
-    
+
     @RequestMapping(value = "/create_user", method = RequestMethod.POST)
-    public String create(@RequestParam("name") String name
-            ,@RequestParam("surname") String surname
-            ,@RequestParam("phone") String phone
-            ,@RequestParam("email") String email
-            ,@RequestParam("password") String password
-            ,@RequestParam("roles") String role) {
-        
-        if(role.equals("ROLE_USER")){
-            studentRepository.save(new Student(name, surname, phone, email, password));
-            User u = new User(email,password);
-            u.setEnabled(true);
-            userRepository.save(u);
-            authoritiesRepository.save(new Authorities(email,role));
-            log.info("USER '"+name+" "+surname+"' created");
-            
-        }else if(role.equals("ROLE_ADMIN")){
-            teacherRepository.save(new Teacher(name, surname, phone, email, password));
-            User u = new User(email,password);
-            u.setEnabled(true);
-            userRepository.save(u);            
-            authoritiesRepository.save(new Authorities(email,role));
-            log.info("ADMIN '"+name+" "+surname+"' created");
+    public String create(@RequestParam("name") String name,
+            @RequestParam("surname") String surname,
+            @RequestParam("phone") String phone,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            @RequestParam("roles") String role) {
+        if (!name.equals("") && !surname.equals("") && !phone.equals("") && !email.equals("") && !password.equals("")) {
+            if (isName(name) && isName(surname) && isTelNumber(phone) && isEmail(email)) {
+                if (role.equals("ROLE_USER")) {
+                    studentRepository.save(new Student(name, surname, phone, email, password));
+                    User u = new User(email, password);
+                    u.setEnabled(true);
+                    userRepository.save(u);
+                    authoritiesRepository.save(new Authorities(email, role));
+                    log.info("USER '" + name + " " + surname + "' created");
+
+                } else if (role.equals("ROLE_ADMIN")) {
+                    teacherRepository.save(new Teacher(name, surname, phone, email, password));
+                    User u = new User(email, password);
+                    u.setEnabled(true);
+                    userRepository.save(u);
+                    authoritiesRepository.save(new Authorities(email, role));
+                    log.info("ADMIN '" + name + " " + surname + "' created");
+                }
+            } else {
+                log.error("Can't create new " + role + ": Wrong input");
+                return "posts/login";
+            }
+        } else {
+            log.error("Can't create new " + role + ": Empty input");
+            return "posts/login";
         }
-        
+
         return "posts/login";
-    }   
-   
+    }
+
     @RequestMapping(value = "/success", method = RequestMethod.GET)
     public String listPosts(Model model, Authentication authentication) {
-        String role="";
-        String name="";
+        String role = "";
+        String name = "";
         for (GrantedAuthority authority : authentication.getAuthorities()) {
             role = authority.getAuthority();
-            name = authentication.getName()+role;
+            name = authentication.getName() + role;
         }
-        if(role.equals("ROLE_USER")){  
+        if (role.equals("ROLE_USER")) {
             model.addAttribute("courses", courseRepository.findAll());
             return "redirect:posts/teststudent";
-        }else if(role.equals("ROLE_ADMIN")){
+        } else if (role.equals("ROLE_ADMIN")) {
             return "redirect:posts/testteacher";
-        }   
+        }
         return "redirect:/?logout";
     }
-    
-    
 
-    
 }
